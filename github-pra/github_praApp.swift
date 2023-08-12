feda2ae79f6abf719d189f75fe3562b839884a55
Kento Oda <kento.oda@relic.co.jp>
2023-08-12 12:57:45 +0900

    FirstCommit

---
+++ github-pra/github_praApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_praApp.swift
+//  github-pra
+//
+//  Created by ODA KENTO on 2023/08/12.
+//
+
+import SwiftUI
+
+@main
+struct github_praApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
